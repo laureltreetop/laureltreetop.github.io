0dbb71897f8babd7a9910daa71eed771cb6f446a
laureltreetop <kozue@treetop.to>
2018-01-01 18:26:07 +0900

    lunr multi languages?

--- assets/js/custom_head.js
+++ assets/js/custom_head.js
@@ -89,3 +89,7 @@ function make_qr_text() {
 
 	$("#qr_text").html('<img src="' + make_url(data['note']) + '"&format=' + data['qrformat'] + '>');
 }
+
+var idx = lunr(function () {
+  this.use(lunr.multiLanguage('en', 'jp'))
+})
