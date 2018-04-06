9a844bd4b8b539c5b07246472069c52ce6892a3b
laureltreetop <kozue@treetop.to>
2018-04-06 18:45:43 +0900

    not end # ?

--- assets/js/custom_head.js
+++ assets/js/custom_head.js
@@ -1,6 +1,17 @@
 // $('a').smoothScroll();
 //$('a[href^="#"]').smoothScroll();
 
+$(function(){
+	$('a[href^="#"]:not([href$="#"])').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
+
 $("h1.page__title").text(
 	$.trim(
 		$("h1.page__title").text()
@@ -22,17 +33,6 @@ $(function() {
 		$('body,html').animate({
 			scrollTop: 0
 		}, 500);
-		return false;
-	});
-});
-
-$(function(){
-	$('a[href^="#"]').click(function(){
-		var speed = 500;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
 		return false;
 	});
 });
