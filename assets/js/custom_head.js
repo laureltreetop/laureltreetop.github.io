88d87c2979be4ff4e97e4d62d819b8a9e9871072
laureltreetop <kozue@treetop.to>
2018-04-06 18:35:33 +0900

    change potision smoothscroll

--- assets/js/custom_head.js
+++ assets/js/custom_head.js
@@ -1,17 +1,6 @@
 // $('a').smoothScroll();
 //$('a[href^="#"]').smoothScroll();
 
-$(function(){
-	$('a[href^="#"]').click(function(){
-		var speed = 500;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
-		return false;
-	});
-});
-
 $("h1.page__title").text(
 	$.trim(
 		$("h1.page__title").text()
@@ -33,6 +22,17 @@ $(function() {
 		$('body,html').animate({
 			scrollTop: 0
 		}, 500);
+		return false;
+	});
+});
+
+$(function(){
+	$('a[href^="#"]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
 		return false;
 	});
 });
