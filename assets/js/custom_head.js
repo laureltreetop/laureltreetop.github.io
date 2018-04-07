6c04363ba20010e5080f26a16e98a435a07d435b
laureltreetop <kozue@treetop.to>
2018-04-07 20:47:01 +0900

    unrelated?

--- assets/js/custom_head.js
+++ assets/js/custom_head.js
@@ -1,6 +1,6 @@
 // $('a').smoothScroll();
 //$('a[href^="#"]:not([href$="#"])').smoothScroll();
-/*
+
 $(function(){
 	$('a[href^="#"]').click(function(){
 		var speed = 500;
@@ -11,7 +11,7 @@ $(function(){
 		return false;
 	});
 });
-*/
+
 $("h1.page__title").text(
 	$.trim(
 		$("h1.page__title").text()
