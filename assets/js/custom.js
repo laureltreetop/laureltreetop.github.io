4a3ea01eb24d0329ab426fe818c7feda74d640d6
laureltreetop <kozue@treetop.to>
2018-04-08 09:50:52 +0900

    change file name and add smooth scroll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,15 @@
 // $('a').smoothScroll();
 //$('a[href^="#"]:not([href$="#"])').smoothScroll();
-
 $(function(){
-	$('a[href^="#"]').click(function(){
-		var speed = 500;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
-		return false;
-	});
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^="#"]').click(function() {
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
 });
 
 $("h1.page__title").text(
