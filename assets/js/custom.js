8b7f843344455c3e161cfc63b661e9f6a01a68b2
laureltreetop <kozue@treetop.to>
2018-04-12 14:53:18 +0900

    unified quote at scroll animation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,10 +2,10 @@
 //$('a[href^="#"]:not([href$="#"])').smoothScroll();
 
 $(function(){
-   $('a[href^="#"]:not([href$="#"])').click(function() {
+   $('a[href^="#"]').click(function() {
       var speed = 400;
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
+      var href= $(this).attr('href');
+      var target = $(href == '#' || href == '' ? 'html' : href);
       var position = target.offset().top;
       $('body,html').animate({scrollTop:position}, speed, 'swing');
       return false;
