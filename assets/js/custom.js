64d92921c0f7eb5105807768949219f8f0131b6f
laureltreetop <kozue@treetop.to>
2019-06-05 20:35:08 +0900

    open external link with target _blank

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,6 +11,16 @@ $(function () {
     });
 });
 
+// https://blog.kaffeekantate.xyz/set-target-blank/
+$(function() {
+	var links = document.links;
+	for (var i = 0; i < links.length; i++) {
+		if (links[i].hostname != window.location.hostname) {
+			links[i].target = '_blank';
+		}
+	}
+});
+
 $(".page__hero--overlay h1.page__title").text(
     $.trim(
         $("h1.page__title").text()
