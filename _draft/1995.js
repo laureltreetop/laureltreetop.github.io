394fc488c996df8fc5dea2cc79d8ff6c675088d4
laureltreetop <kozue@treetop.to>
2019-06-21 23:07:03 +0900

    1995 infinite scroll version

--- _draft/1995.js
+++ _draft/1995.js
@@ -1,57 +1,72 @@
-function hitTemplate(hit) {
-var resultStr = `${hit.text.join('')}`;
-     return `
-    <dl>
-    <dt><a href="${hit.url}" target="_blank">${hit._highlightResult.title.value}</a></dt>
-    <dd>
-        ${resultStr.slice(0, 160)}&hellip;
-    </dd>
-    </dl>
-  `;
-}
-
 const search = instantsearch({
-    appId: 'IA90805VYI',
-    apiKey: '9e30d633e7ae5597b0ddb7744f621017',
+    searchClient: algoliasearch('IA90805VYI', '9e30d633e7ae5597b0ddb7744f621017'),
     indexName: 'index',
     routing: true,
-    searchParameters: {
-        hitsPerPage: 5
-    }
 });
 
-// initialize SearchBox
 search.addWidget(
     instantsearch.widgets.searchBox({
         container: '#searchbox',
         placeholder: 'ver.1995を検索…',
         poweredBy: true,
-    })
-);
-
-// initialize hits widget
-search.addWidget(
-    instantsearch.widgets.hits({
-        container: '#hits',
-        templates: {
-            empty: "キーワード<em>「{{query}}」</em>では探せませんでした。",
-            item: function(hit) {
-                return hitTemplate(hit);
-            }
+        cssClasses: {
+            form: 'search-nagisa',
         },
     })
 );
 
-// initialize pagination
 search.addWidget(
-    instantsearch.widgets.pagination({
-        container: '#pagination',
-        maxPages: 20,
-        // default is to scroll to 'body', here we disable this behavior
-        scrollTo: false,
-		showFirstLast: true,
-		collapsible: true,
-		autoHideContainer: true,
+    instantsearch.widgets.poweredBy({
+        container: '#powered-by',
+        cssClasses: {
+            root: 'powered-by-nagisa',
+        },
+    })
+);
+
+search.addWidget(
+    instantsearch.widgets.stats({
+        container: '#stats',
+        templates: {
+            text(data) {
+                let count = '';
+
+                if (data.hasManyResults) {
+                    count += `${data.nbHits} results`;
+                } else if (data.hasOneResult) {
+                    count += `1 result`;
+                } else {
+                    count += `no result`;
+                }
+                if (data.query == "") {
+                    document.getElementById("stats").style.visibility="hidden";
+                    document.getElementById("hits").style.visibility="hidden";
+                } else {
+                    document.getElementById("stats").style.visibility="visible";
+                    document.getElementById("hits").style.visibility="visible";
+                }
+                return `${count} found in ${data.processingTimeMS}ms`;
+            },
+        },
+    })
+);
+
+search.addWidget(
+    instantsearch.widgets.infiniteHits({
+        container: '#hits',
+        cssClasses: {
+            item: 'infinite-nagisa',
+        },
+        templates: {
+            empty: "キーワード<em>「{{query}}」</em>では探せませんでした。",
+            item(hit) {
+                return `
+                <dt><a href="${hit.url}" target="_blank">${hit._highlightResult.title.value}</a></dt>
+                <dd>${hit._snippetResult.content.value}</dd>
+                `;
+            }
+        }
+
     })
 );
 
